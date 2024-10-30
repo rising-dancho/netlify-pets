50c4256f8dc0c436e10db73348ad1fcaac2968f8
Jose Perez Jr <zenlaws02@gmail.com>
2024-10-31 04:25:53 +0800

    feat: in progress, working with cookies for the first time

--- netlify/functions/adminDashboard.js
+++ netlify/functions/adminDashboard.js
@@ -1,10 +1,27 @@
-const handler = async () => {
+const cookie = require('cookie');
+
+const handler = async (event) => {
+  // console.log(event.headers.cookie);
+
+  const incomingCookie = cookie.parse(event.headers.cookie || '');
+  if (
+    incomingCookie?.petadoption == 'asdasdasdasdEADFACDASDASdasd!13224324sd'
+  ) {
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ success: true }),
+    };
+  }
+
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ success: true }),
+    body: JSON.stringify({ success: false }),
   };
 };
 
