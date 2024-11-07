fa87e988550dc7eb13260e4444488284ec43e58b
adfinem jose perez jr <zenlaws02@gmail.com>
2024-11-08 02:04:33 +0800

    feat: in progress, working on the backend of the add pet page

--- netlify/functions/adminDashboard.js
+++ netlify/functions/adminDashboard.js
@@ -1,13 +1,12 @@
 const { MongoClient } = require('mongodb');
+const isAdmin = require('../../our-library/isAdmin');
+
 const cookie = require('cookie');
 
 const handler = async (event) => {
   // console.log(event.headers.cookie);
 
-  const incomingCookie = cookie.parse(event.headers.cookie || '');
-  if (
-    incomingCookie?.petadoption == 'asdasdasdasdEADFACDASDASdasd!13224324sd'
-  ) {
+  if (isAdmin(event)) {
     // making a connection to the mongodb database
     const client = new MongoClient(process.env.CONNECTION_STRING);
     await client.connect();
