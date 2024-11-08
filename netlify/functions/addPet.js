c1d1f1ead8696143e77f46fb15e7944eb9967489
adfinem jose perez jr <zenlaws02@gmail.com>
2024-11-08 23:39:13 +0800

    feat: in progress, added validation for pet birthYear and species on the Add pet page

--- netlify/functions/addPet.js
+++ netlify/functions/addPet.js
@@ -5,12 +5,28 @@ const handler = async (event) => {
   const body = JSON.parse(event.body);
   console.log(body);
 
+  let pet = {
+    name: body.name,
+    species: body.species,
+    description: body.description,
+    birthYear: new Data().getFullYear(),
+  }; // add current year user didn't type in a birth year
+
+  // checking for the birthYear's validity
+  if (body.birthYear > 999 && body.birthYear < 9999) {
+    pet.birthYear = body.birthYear;
+  }
+
+  if(pet.species !== "Cat" && pet.species !== "Dog"){
+    pet.species = "Dog"
+  }
+
   // isAdmin is a separate reusable function
   if (isAdmin(event)) {
     // making a connection to the mongodb database
     const client = await getDbClient();
     // actually save into database
-    const pets = await client.db().collection('pets').insertOne(body);
+    const pets = await client.db().collection('pets').insertOne(pet);
     // client.close();
 
     return {
