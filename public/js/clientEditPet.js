9512f817306b9cfd637fe47f8d27779a5c7e1718
adfinem jose perez jr <zenlaws02@gmail.com>
2024-11-19 11:21:43 +0800

    feat: in progress, adding edit pet feature

--- public/js/clientEditPet.js
+++ public/js/clientEditPet.js
@@ -1,3 +1,17 @@
 const urlVariables = new URLSearchParams(window.location.search);
 const id = urlVariables.get('id');
-alert(id);
+// alert(id);
+
+async function getEditPet() {
+  const ourPromise = await fetch('/.netlify/functions/getSingularPet', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ id: id }),
+  });
+
+  const pet = await ourPromise.json();
+  alert(pet);
+  console.log(pet);
+}
+
+getEditPet();
